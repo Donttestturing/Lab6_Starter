ee526dd1f9869687f62db040be8bd42cd2960fea
Brendan Devlin <52110466+Donttestturing@users.noreply.github.com>
2022-11-01 21:47:28 -0700

    Fix bug on page load with no data

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -42,7 +42,7 @@ function addRecipesToDocument(recipes) {
   //            create a <recipe-card> element for each one, and populate
   //            each <recipe-card> with that recipe data using element.data = ...
   //            Append each element to <main>
-  if(recipes == null){
+  if(recipes === null){
     return;
   }
 
